eb59f39ffb458d46ef8416edd49b3aed4ddad598
Igor Stanisavljevic <stanisavljevic.igor@proton.me>
2026-03-14 15:52:20 +0100

    jovanas changes/ some of mine

--- views/home.php
+++ views/home.php
@@ -23,12 +23,12 @@
         }, t)
     }
     showAnnouncements([
-       "US Warehouse Now Restocked! Enjoy domestic shipping on a variety of products - fast and reliable.🚀",
+       "US Warehouse Now Restocked! AWESOME PRICES AND FREE DOMESTIC SHIPPING - fast and reliable.🚀",
         //"💥 Get $25 USD OFF on ALL orders over $130 USD! 💥 Use code ALL25 - limited-time offer! Discounts cannot be combined.",
     "🔥All payment options available!🔥",
         //"🎁 Free samples with your next order when you leave us a review!👉 Just send us your review link, and the freebies are yours!🎁",
         "NO HIDDEN FEES. 24/7 SUPPORT. GUARANTEED FREE SHIPPING.",
-        "🚀 Get our Premium Moda Mak·er and Armoda Do·er formulas with SUPER-FAST shipping!🚀 ",
+        "🚀 Get our Premium Moda Mak·er and Armoda Do·er formulas with SUPER-FAST shipping! USE THE CODE 10OFF TO GET 10% OFF!🚀 ",
     //Due to the Easter holiday, some delays may occur in processing and shipping. Thank you for your patience.",
     ], 4000);
 </script>
